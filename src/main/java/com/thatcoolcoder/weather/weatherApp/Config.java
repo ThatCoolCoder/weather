4182ad11886ee39b16b5b3dac00f175dcd714f46
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2022-01-12 10:30:01 +1030

    Remove unneeded import

--- src/main/java/com/thatcoolcoder/weather/weatherApp/Config.java
+++ src/main/java/com/thatcoolcoder/weather/weatherApp/Config.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.ExecutionException;
 
 import org.json.*;
 
